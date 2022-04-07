96b9a19a971c957187d0a18b227b255ce2a18614
Nobuyuki Ikeda <mail@nobuyuki-ikeda.com>
2022-04-07 18:01:47 +0900

    トップレイアウト修正

--- src/pages/top/Top.tsx
+++ src/pages/top/Top.tsx
@@ -4,10 +4,46 @@ import Kv from './kv/Kv';
 import Sketch from './sketch/Sketch';
 import './top.scss';
 
-export default class Top extends React.Component {
+type State = {
+  loaderOpacity: number;
+  loaderDisplay: 'block' | 'none';
+};
+
+export default class Top extends React.Component<{}, State> {
+  state: State = {
+    loaderOpacity: 1,
+    loaderDisplay: 'block',
+  };
+  get loaderStyle() {
+    return {
+      opacity: this.state.loaderOpacity,
+      display: this.state.loaderDisplay,
+    };
+  }
+
+  componentDidMount() {
+    this.hideLoading();
+  }
+
+  hideLoading() {
+    this.setState({
+      ...this.state,
+      loaderOpacity: 0,
+    });
+    setTimeout(() => {
+      this.setState({
+        ...this.state,
+        loaderDisplay: 'none',
+      });
+    }, 400);
+  }
+
   render() {
     return (
       <>
+        <div className="loader" style={this.loaderStyle}>
+          <p>Now loading...</p>
+        </div>
         <header />
         <main>
           <Kv />
--- src/pages/top/kv/Kv.tsx
+++ src/pages/top/kv/Kv.tsx
@@ -1,66 +1,17 @@
 import React from 'react';
 import './kv.scss';
 
-type State = {
-  loaderOpacity: number;
-  loaderDisplay: 'block' | 'none';
-  kvOpacity: number;
-};
-
-export default class Kv extends React.Component<{}, State> {
-  state: State = {
-    loaderOpacity: 1,
-    loaderDisplay: 'block',
-    kvOpacity: 0,
-  };
-  get loaderStyle() {
-    return {
-      opacity: this.state.loaderOpacity,
-      display: this.state.loaderDisplay,
-    };
-  }
-
-  componentDidMount() {
-    this.hideLoading();
-  }
-
-  hideLoading() {
-    this.setState({
-      ...this.state,
-      loaderOpacity: 0,
-    });
-    setTimeout(() => {
-      this.setState({
-        ...this.state,
-        loaderDisplay: 'none',
-      });
-      this.showKv();
-    }, 400);
-  }
-
-  showKv() {
-    this.setState({
-      ...this.state,
-      kvOpacity: 1,
-    });
-  }
-
+export default class Kv extends React.Component {
   render() {
     return (
       <section className="l-section l-section--home">
-        <div className="loader" style={this.loaderStyle}>
-          <p>Now loading...</p>
-        </div>
         <div className="hero">
           <div className="kv"></div>
           <div className="kv__canvas"></div>
-          <div
-            className="scroll-icon"
-            style={{ opacity: this.state.kvOpacity }}
-          >
+          <div className="scroll-icon">
             <span></span>
           </div>
-          <div className="catch" style={{ opacity: this.state.kvOpacity }}>
+          <div className="catch">
             <h1>This is my portfolio site</h1>
             <h2>Nobuyuki Ikeda</h2>
             <p>web developer</p>
